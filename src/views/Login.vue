259f0a60cc3a02d1cf5df29ae06ca41e7fdd44f5
Sagar <sagar1309@live.com>
2019-12-06 01:21:16 +0530

    adds some basic files

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+      LoginPage
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+<style lang="scss">
+.login {
+    // 
+}
+</style>
