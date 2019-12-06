267216311a43c538bee9896c2c19f7a86ef68c0b
Sagar <sagar1309@live.com>
2019-12-07 02:14:04 +0530

    login page ready

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,39 @@
 <template>
 	<v-content>
 		<v-container class="fill-height" fluid>
-            Login Page
+			<v-row align="center" justify="center">
+				<v-col cols="12" sm="8" md="4">
+					<v-card class="elevation-12">
+						<v-toolbar color="primary" dark flat>
+							<v-toolbar-title>Login form</v-toolbar-title>
+							<v-spacer />
+						</v-toolbar>
+						<v-card-text>
+							<v-form>
+								<v-text-field
+									label="Login"
+									name="login"
+									prepend-icon="fas fa-user-shield"
+									type="text"
+								/>
+
+								<v-text-field
+									id="password"
+									label="Password"
+									name="password"
+									prepend-icon="fas fa-lock"
+									type="password"
+									@keyup.enter="$router.push({ name: 'home' })"
+								/>
+							</v-form>
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer />
+							<v-btn color="primary" @click="$router.push({ name: 'home' })">Login</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-col>
+			</v-row>
 		</v-container>
 	</v-content>
 </template>
