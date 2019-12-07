9ef1ddd7a543efa9b660405cd8af71f8af36198c
Sagar <sagar1309@live.com>
2019-12-07 16:12:23 +0530

    Home passes routes to its NavigationDrawer items and wraps a container around router-view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,61 @@
 <template>
-	<div class="home">
-		<NavigationDrawer :items="items" />
+  <div class="home">
+    <NavigationDrawer :items="items" />
 
-		<HomeNavbar :app-name="appName" />
+    <HomeNavbar :app-name="appName" />
 
-		<v-content>
-			<router-view></router-view>
-		</v-content>
-	</div>
+    <v-content>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-content>
+  </div>
 </template>
 
 <script>
-import NavigationDrawer from '@/components/NavigationDrawer.vue';
-import HomeNavbar from '@/components/HomeNavbar.vue';
+import NavigationDrawer from "@/components/NavigationDrawer.vue";
+import HomeNavbar from "@/components/HomeNavbar.vue";
 import config from "@/config.js";
 
 export default {
-	name: "home",
-	components: {
-		NavigationDrawer,
-		HomeNavbar
-	},
-	data() {
-		return {
-			items: [
-				{ title: "Dashboard", icon: "fas fa-tachometer-alt" },
-				{ title: "Bill", icon: "fas fa-file-invoice" },
-				{ title: "Chalan", icon: "fas fa-receipt" },
-				{ title: "Products", icon: "fas fa-file-powerpoint" },
-				{ title: "History", icon: "fas fa-history" },
-				{ title: "Reports", icon: "fas fa-clipboard-list" },
-				{ title: "Settings", icon: "fas fa-cogs" }
-			],
-		};
-	},
-	computed: {
-		appName() {
-			return config.appTitle();
-		}
-	},
-	methods: {
-		// 
-	}
+  name: "Home",
+  components: {
+    NavigationDrawer,
+    HomeNavbar
+  },
+  data() {
+    return {
+      items: [
+        {
+          title: "Dashboard",
+          icon: "fas fa-tachometer-alt",
+          route: "dashboard"
+        },
+        { title: "Bill", icon: "fas fa-file-invoice", route: "bill" },
+        { title: "Chalan", icon: "fas fa-receipt", route: "chalan" },
+        {
+          title: "Products",
+          icon: "fas fa-file-powerpoint",
+          route: "products"
+        },
+        { title: "History", icon: "fas fa-history", route: "history" },
+        { title: "Reports", icon: "fas fa-clipboard-list", route: "reports" },
+        { title: "Settings", icon: "fas fa-cogs", route: "settings" }
+      ]
+    };
+  },
+  computed: {
+    appName() {
+      return config.appTitle();
+    }
+  },
+  methods: {
+    //
+  }
 };
 </script>
 <style lang="scss">
 .home {
-	min-height: 94vh;
+  min-height: 94vh;
 }
 </style>
