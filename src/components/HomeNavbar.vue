9343453c30ee6a1813de1ffe9d2c5bd6ad1da142
Sagar <sagar1309@live.com>
2019-12-07 16:14:22 +0530

    a v-menu for logout in HomeNavbar

--- src/components/HomeNavbar.vue
+++ src/components/HomeNavbar.vue
@@ -1,16 +1,30 @@
 <template>
-	<v-app-bar app color="deep-purple accent-4" dense dark class="home-navbar">
-		<v-toolbar-title>{{ appName }}</v-toolbar-title>
-		<v-spacer></v-spacer>
+  <v-app-bar app color="deep-purple accent-4" dense dark class="home-navbar">
+    <v-toolbar-title>{{ appName }}</v-toolbar-title>
+    <v-spacer></v-spacer>
 
-		<v-btn icon>
-			<v-icon>fas fa-user-circle</v-icon>
-		</v-btn>
-	</v-app-bar>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-icon>fas fa-user-circle</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item @click="logout">
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
 </template>
 <script>
 export default {
-    name: 'home-navbar',
-    props: ["appName"]
-}
+  name: "HomeNavbar",
+  props: ["appName"],
+  methods: {
+    logout() {
+      //
+    }
+  }
+};
 </script>
