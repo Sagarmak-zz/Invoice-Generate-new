51bdcc4870a5a0fb26de7e56987f037c2bfbdd11
Sagar <sagar1309@live.com>
2019-12-07 16:13:03 +0530

    accepts item.route and redirects to its page

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,42 +1,48 @@
 <template>
-	<v-navigation-drawer app expand-on-hover permanent>
-		<v-list-item>
-			<v-list-item-avatar>
-				<v-icon>fas fa-user-circle</v-icon>
-			</v-list-item-avatar>
+  <v-navigation-drawer app expand-on-hover permanent>
+    <v-list-item>
+      <v-list-item-avatar>
+        <v-icon>fas fa-user-circle</v-icon>
+      </v-list-item-avatar>
 
-			<v-list-item-title>John Leider</v-list-item-title>
-		</v-list-item>
+      <v-list-item-title>John Leider</v-list-item-title>
+    </v-list-item>
 
-		<v-divider></v-divider>
+    <v-divider></v-divider>
 
-		<v-list dense>
-			<v-list-item class="my-3" v-for="item in items" :key="item.title" link>
-				<v-list-item-icon>
-					<v-icon>{{ item.icon }}</v-icon>
-				</v-list-item-icon>
+    <v-list dense>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        class="my-3"
+        link
+        :to="item.route"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
 
-				<v-list-item-content>
-					<v-list-item-title>{{ item.title }}</v-list-item-title>
-				</v-list-item-content>
-			</v-list-item>
-		</v-list>
-	</v-navigation-drawer>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 <script>
 export default {
-	name: "navigation-drawer",
-	props: ["items"],
-	data() {
-		return {
-			//
-		};
-	},
-	computed: {
-		// 
-	},
-	methods: {
-		// 
-	}
+  name: "NavigationDrawer",
+  props: ["items"],
+  data() {
+    return {
+      //
+    };
+  },
+  computed: {
+    //
+  },
+  methods: {
+    //
+  }
 };
 </script>
