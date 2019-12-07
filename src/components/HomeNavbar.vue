cf687ad7c2eae918abad87dc8df1788eef6f4b3f
Sagar <sagar1309@live.com>
2019-12-07 14:31:33 +0530

    adds Navbar and Navigation Drawer components

---
+++ src/components/HomeNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-app-bar app color="deep-purple accent-4" dense dark class="home-navbar">
+		<v-toolbar-title>{{ appName }}</v-toolbar-title>
+		<v-spacer></v-spacer>
+
+		<v-btn icon>
+			<v-icon>fas fa-user-circle</v-icon>
+		</v-btn>
+	</v-app-bar>
+</template>
+<script>
+export default {
+    name: 'home-navbar',
+    props: ["appName"]
+}
+</script>
