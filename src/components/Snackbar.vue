dab1fb6b8904b3ae662cb95ad8f073e42c2905a5
Sagar <sagar1309@live.com>
2019-12-09 01:55:02 +0530

    adds a new component for notifications, Snackbar

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-snackbar v-model="status" top>
+      <span>
+        {{ getSnackbarData.text }}
+      </span>
+      <v-icon v-if="getSnackbarData.icon" flat>
+        {{ getSnackbarData.icon }}
+      </v-icon>
+    </v-snackbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "Snackbar",
+  data: () => ({
+    status: false
+  }),
+  computed: {
+    snackbar() {
+      return this.$store.getters.snackbar;
+    },
+    getSnackbarData() {
+      return {
+        status: this.snackbar.status || true,
+        timeout: this.snackbar.timeout || 4000,
+        color: this.snackbar.color || "success",
+        icon: this.snackbar.icon || "",
+        text: this.snackbar.text || ""
+      };
+    }
+  }
+};
+</script>
