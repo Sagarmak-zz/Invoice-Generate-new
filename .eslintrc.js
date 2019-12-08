f14840ba34eb24081f1ab8ed9ff52119d94d7552
Sagar <sagar1309@live.com>
2019-12-09 02:56:20 +0530

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,11 @@ module.exports = {
   // extends: ["plugin:vue/essential", "@vue/prettier"],
   extends: [
     "plugin:vue/recommended",
-    "eslint:recommended",
     "prettier/vue",
     "plugin:prettier/recommended"
   ],
   rules: {
+    "vue/no-side-effects-in-computed-properties": "off",
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
