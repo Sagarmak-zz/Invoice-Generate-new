5031ce76a0944fe183f72206676a23b96306dedc
Sagar <sagar1309@live.com>
2019-12-13 01:33:29 +0530

    install chartjs for Dashboard

---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,23 @@
+<script>
+import { Pie } from "vue-chartjs";
+
+export default {
+  name: "PieChart",
+  extends: Pie,
+  //   props: ["chartData", "chartOptions"],
+  mounted() {
+    this.renderChart(
+      {
+        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
+        datasets: [
+          {
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+            data: [40, 20, 80, 10]
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  }
+};
+</script>
