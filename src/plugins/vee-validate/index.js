81481f9ce7da955b4b27852233d5f12418634053
Sagar <sagar1309@live.com>
2019-12-27 01:37:27 +0530

    fix validation rules

--- src/plugins/vee-validate/index.js
+++ src/plugins/vee-validate/index.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import { extend } from "vee-validate";
-import { required, email, alpha_num, min, max, length, integer } from "vee-validate/dist/rules";
+import { required, email, alpha_num, min_value, max_value, integer } from "vee-validate/dist/rules";
 
 // register VeeValidate rules
 extend("required", required);
 extend("email", email);
 extend("alpha_num", alpha_num);
-extend("min", min);
-extend("max", max);
-extend("length", length);
+extend("min_value", min_value);
+extend("max_value", max_value);
 extend("integer", integer);
