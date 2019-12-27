bd21cbaa2e3b369c04251419ab5d86a43478c40d
Sagar <sagar1309@live.com>
2019-12-28 03:37:49 +0530

    adds more rules in vee-validate

--- src/plugins/vee-validate/index.js
+++ src/plugins/vee-validate/index.js
@@ -1,6 +1,16 @@
 import Vue from "vue";
 import { extend } from "vee-validate";
-import { required, email, alpha_num, min_value, max_value, length, integer } from "vee-validate/dist/rules";
+import {
+  required,
+  email,
+  alpha_num,
+  min_value,
+  max_value,
+  length,
+  integer,
+  regex,
+  required_if
+} from "vee-validate/dist/rules";
 
 // register VeeValidate rules
 extend("required", required);
@@ -10,3 +20,5 @@ extend("min_value", min_value);
 extend("max_value", max_value);
 extend("length", length);
 extend("integer", integer);
+extend("regex", regex);
+extend("required_if", required_if);
