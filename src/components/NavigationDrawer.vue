f5fe73c6da632d6ef45a30718ccde6f3bff8f050
Sagar Makhija <sagar1309@live.com>
2020-05-09 17:26:40 +0530

    fix navigation draver show on hover

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,27 +1,40 @@
 <template>
-  <v-navigation-drawer app clipped expand-on-hover>
-    <v-list-item>
-      <v-list-item-avatar>
-        <v-icon>fas fa-user-circle</v-icon>
-      </v-list-item-avatar>
+  <v-card>
+    <v-navigation-drawer v-model="drawer" app clipped :mini-variant.sync="mini" permanent>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-icon>fas fa-user-circle</v-icon>
+        </v-list-item-avatar>
 
-      <v-list-item-title>{{ userDetails.username }}</v-list-item-title>
-    </v-list-item>
+        <v-list-item-title>{{ userDetails.username }}</v-list-item-title>
 
-    <v-divider></v-divider>
-
-    <v-list dense>
-      <v-list-item v-for="item in items" :key="item.title" color="primary" class="my-3" link :to="{ name: item.route }">
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
+        <v-btn icon @click="mini = !mini">
+          <v-icon>fas fa-chevron-left</v-icon>
+        </v-btn>
       </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          color="primary"
+          class="my-3"
+          link
+          :to="{ name: item.route }"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
 </template>
 <script>
 export default {
@@ -34,7 +47,8 @@ export default {
   },
   data() {
     return {
-      //
+      drawer: true,
+      mini: true
     };
   },
   computed: {
