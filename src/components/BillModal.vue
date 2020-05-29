c105e3aa9b2c50fdf01350786ce17ef349b46fae
Sagar Makhija <sagar1309@live.com>
2020-05-29 18:38:44 +0530

    adds cancel button at the top and generates invoice pdf on Print button click

--- src/components/BillModal.vue
+++ src/components/BillModal.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="bill-modal">
-    <v-dialog v-model="dialog" width="23cm" :loading="loading">
+    <v-dialog v-model="dialog" width="23cm" :loading="loading" light persistent>
       <v-card>
         <v-card-title>
           <!-- Dialog title if a title prop found -->
           <div class="headline">{{ title }}</div>
           <v-spacer></v-spacer>
-          <v-btn class="primary">Print</v-btn>
+          <v-btn class="primary" @click="printBill">Print</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="$emit('bill-modal', false)">Close</v-btn>
           <!-- Dialog title if a title slot provided -->
           <slot name="title" />
         </v-card-title>
         <v-card-text>
-          <PrintBill :data="data" />
+          <PrintBill ref="invoice" :data="data" />
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -19,6 +21,8 @@
 </template>
 <script>
 import PrintBill from "./PrintBill";
+import html2pdf from "html2pdf.js";
+
 export default {
   name: "BillModal",
   components: {
@@ -42,6 +46,22 @@ export default {
     return {
       dialog: true
     };
+  },
+  methods: {
+    printBill() {
+      const element = this.$refs.invoice.$refs.printable_invoice;
+      console.log("printBill -> element", element);
+      const options = {
+        html2canvas: {
+          scrollX: 0,
+          scrollY: 0
+        }
+      };
+      html2pdf()
+        .from(element)
+        .set(options)
+        .save("filename");
+    }
   }
 };
 </script>
